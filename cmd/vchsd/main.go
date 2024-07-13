026d791516c24df66760661cca1748ae51008f68
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-07-13 17:23:28 +0800

    create vchs entrypoint

--- cmd/vchsd/main.go
+++ cmd/vchsd/main.go
@@ -1,5 +1,105 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"vcassist-backend/lib/configuration"
+	"vcassist-backend/services/gradesnapshots"
+	"vcassist-backend/services/linker"
+	powerschoold "vcassist-backend/services/powerschool"
+	"vcassist-backend/services/studentdata/api/apiconnect"
+	"vcassist-backend/services/vchs"
+	"vcassist-backend/services/vchsmoodle"
 
+	"github.com/dgraph-io/badger/v4"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+func fatalerr(message string, err error) {
+	slog.Error(message, "err", err.Error())
+	os.Exit(1)
+}
+
+type DatabaseConfig struct {
+	GradeSnapshot configuration.Libsql `json:"grade_snapshot"`
+	Linker        configuration.Libsql `json:"linker"`
+	Powerschoold  configuration.Libsql `json:"powerschoold"`
+	VchsMoodle    configuration.Libsql `json:"vchs_moodle"`
+	Self          configuration.Libsql `json:"self"`
+}
+
+type Config struct {
+	Database DatabaseConfig `json:"database"`
+}
+
+func main() {
+	config, err := configuration.ReadConfig[Config]("config.json")
+	if err != nil {
+		fatalerr("failed to read config", err)
+	}
+
+	var db *sql.DB
+	db, err = config.Database.GradeSnapshot.OpenDB()
+	if err != nil {
+		fatalerr("failed to open gradesnapshot database", err)
+	}
+	gradesnapshotService := gradesnapshots.NewService(db)
+
+	db, err = config.Database.Linker.OpenDB()
+	if err != nil {
+		fatalerr("failed to open linker database", err)
+	}
+	linkerService := linker.NewService(db)
+
+	db, err = config.Database.Powerschoold.OpenDB()
+	if err != nil {
+		fatalerr("failed to open powerschoold database", err)
+	}
+	powerschooldService := powerschoold.NewService(
+		db,
+		"https://vcsnet.powerschool.com",
+		powerschoold.OAuthConfig{
+			BaseLoginUrl: "https://accounts.google.com/o/oauth2/v2/auth",
+			RefreshUrl:   "https://oauth2.googleapis.com/token",
+			ClientId:     "162669419438-egansm7coo8n7h301o7042kad9t9uao9.apps.googleusercontent.com",
+		},
+	)
+
+	moodleCache, err := badger.Open(badger.DefaultOptions("moodle-cache.db"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err = config.Database.VchsMoodle.OpenDB()
+	if err != nil {
+		fatalerr("failed to open moodle database", err)
+	}
+	vchsmoodleService := vchsmoodle.NewService(db, moodleCache)
+
+	db, err = config.Database.Self.OpenDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	service := vchs.NewService(
+		db,
+		powerschooldService,
+		vchsmoodleService,
+		linkerService,
+		gradesnapshotService,
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle(apiconnect.NewStudentDataServiceHandler(service))
+
+	slog.Info("listening to gRPC...", "port", 9111)
+	err = http.ListenAndServe(
+		"127.0.0.1:8111",
+		h2c.NewHandler(mux, &http2.Server{}),
+	)
+	if err != nil {
+		fatalerr("failed to listen on port 9111", err)
+	}
 }
