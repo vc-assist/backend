06b90ea489f17edda44ee76b994f087921c15415
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-08-11 18:31:45 -0700

    don't use embedded replica mode

--- lib/configuration/libsql/config_libsql.go
+++ lib/configuration/libsql/config_libsql.go
@@ -4,16 +4,11 @@ import (
 	"database/sql"
 	"fmt"
 	"net/url"
-	"os"
 	devenv "vcassist-backend/dev/env"
 
-	"github.com/tursodatabase/go-libsql"
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
-// if only "file" is specified, it will open the file local-only mode
-// if only "url" is specified, it will open the database in remote-only mode
-// if "url" is specified alongside "file" it will open the database in embedded-replica mode
 type Struct struct {
 	File      string `json:"file"`
 	Url       string `json:"url"`
@@ -32,32 +27,13 @@ func (config Struct) OpenDB() (*sql.DB, error) {
 		return sql.Open("libsql", fmt.Sprintf("file:%s", dbpath))
 	}
 
-	if config.File == "" {
-		values := url.Values{}
-		if config.AuthToken != "" {
-			values.Add("authToken", config.AuthToken)
-		}
-		db, err := sql.Open("libsql", config.Url+"?"+values.Encode())
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to open db %s: %s", config.Url, err)
-			os.Exit(1)
-		}
-		return db, nil
-	}
-
-	var opts []libsql.Option
+	values := url.Values{}
 	if config.AuthToken != "" {
-		opts = []libsql.Option{
-			libsql.WithAuthToken(config.AuthToken),
-		}
+		values.Add("authToken", config.AuthToken)
 	}
-	connector, err := libsql.NewEmbeddedReplicaConnector(
-		config.File,
-		config.Url,
-		opts...,
-	)
+	db, err := sql.Open("libsql", config.Url+"?"+values.Encode())
 	if err != nil {
 		return nil, err
 	}
-	return sql.OpenDB(connector), nil
+	return db, nil
 }
