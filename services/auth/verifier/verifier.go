0cda5503ab34b19c76060c70f8c67e8e79ec2448
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-08-11 18:20:17 -0700

    add uniqueLoginCounter

--- services/auth/verifier/verifier.go
+++ services/auth/verifier/verifier.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"vcassist-backend/lib/timezone"
 	"vcassist-backend/services/auth/db"
 
 	"go.opentelemetry.io/otel"
@@ -11,6 +12,23 @@ import (
 )
 
 var tracer = otel.Tracer("vcassist.services.auth.verifier")
+var meter = otel.Meter("vcassist.services.auth.verifier")
+
+var uniqueLoginCounter, _ = meter.Int64Counter("uniqueLoginCounter")
+var loggedInToday = map[string]struct{}{}
+var todaysDate = timezone.Now().Day()
+
+func countLogin(ctx context.Context, email string) {
+	if timezone.Now().Day() != todaysDate {
+		loggedInToday = map[string]struct{}{}
+	}
+	_, alreadyLoggedIn := loggedInToday[email]
+	if alreadyLoggedIn {
+		return
+	}
+	uniqueLoginCounter.Add(ctx, 1)
+	loggedInToday[email] = struct{}{}
+}
 
 type Verifier struct {
 	qry *db.Queries
@@ -35,6 +53,8 @@ func (v Verifier) VerifyToken(ctx context.Context, token string) (db.User, error
 		span.SetStatus(codes.Error, "got unexpected error while reading token")
 		return db.User{}, err
 	}
+
+	countLogin(ctx, email)
 
 	return db.User{Email: email}, nil
 }
