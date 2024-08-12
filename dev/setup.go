f282689b0e14b1dccfe25838707027e4b40adaa3
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-08-12 11:15:47 -0700

    remove db creation behavior from devenv

--- dev/setup.go
+++ dev/setup.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log/slog"
 	"os"
 	"os/exec"
-	"path/filepath"
-	devenv "vcassist-backend/dev/env"
-
-	authdb "vcassist-backend/services/auth/db"
-	gradesnapshotsdb "vcassist-backend/services/gradesnapshots/db"
-	keychaindb "vcassist-backend/services/keychain/db"
-	linkerdb "vcassist-backend/services/linker/db"
-	powerservicedb "vcassist-backend/services/powerservice/db"
-	vcsdb "vcassist-backend/services/vcs/db"
 
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 	_ "modernc.org/sqlite"
@@ -45,56 +35,6 @@ func CreateLocalStack() error {
 	}
 	cmd("docker", "compose", "up", "-d")
 	return os.Chdir("../..")
-}
-
-func createDb(filename, schema string) error {
-	authPath, err := devenv.ResolvePath(filepath.Join("<dev_state>", filename))
-	if err != nil {
-		return err
-	}
-
-	_, err = os.Stat(authPath)
-	if err == nil {
-		fmt.Println("database already created at", authPath)
-		return nil
-	}
-
-	fmt.Println("creating database at", authPath)
-	db, err := sql.Open("sqlite", authPath)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-	_, err = db.Exec(schema)
-	return err
-}
-
-func CreateEmptyServiceDBs() error {
-	err := createDb("auth_service.db", authdb.Schema)
-	if err != nil {
-		return err
-	}
-	err = createDb("gradesnapshots_service.db", gradesnapshotsdb.Schema)
-	if err != nil {
-		return err
-	}
-	err = createDb("keychain_service.db", keychaindb.Schema)
-	if err != nil {
-		return err
-	}
-	err = createDb("linker_service.db", linkerdb.Schema)
-	if err != nil {
-		return err
-	}
-	err = createDb("powerservice_service.db", powerservicedb.Schema)
-	if err != nil {
-		return err
-	}
-	err = createDb("vcs_service.db", vcsdb.Schema)
-	if err != nil {
-		return err
-	}
-	return err
 }
 
 func PrintConfigLocations() {
