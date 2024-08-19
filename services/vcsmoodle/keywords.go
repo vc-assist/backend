e896c6f4f2e751c1dca42747ea0517c4ab99d782
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-08-18 19:08:32 -0700

    refactor textutil

--- services/vcsmoodle/keywords.go
+++ services/vcsmoodle/keywords.go
@@ -1,29 +1,5 @@
 package vcsmoodle
 
-import (
-	"regexp"
-	"strings"
-)
-
-var whitespaceRegex = regexp.MustCompile(`\s+`)
-
-func NormalizeName(name string) string {
-	name = strings.ToLower(name)
-	name = strings.Trim(name, " \n\t")
-	name = whitespaceRegex.ReplaceAllString(name, "")
-	return name
-}
-
-func MatchName(name string, matchers []string) bool {
-	name = NormalizeName(name)
-	for _, m := range matchers {
-		if strings.Contains(name, m) {
-			return true
-		}
-	}
-	return false
-}
-
 var blacklistCourseKeywords = []string{
 	"christianservice",
 	"ipad",
