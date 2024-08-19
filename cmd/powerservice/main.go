ab839a0c0dffe8c058f591de7b09f9b0b21ece82
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-08-18 19:36:39 -0700

    remove a bunch of unused features from powerservice, including the whole db dir

--- cmd/powerservice/main.go
+++ cmd/powerservice/main.go
@@ -4,13 +4,11 @@ import (
 	"context"
 	"net/http"
 	"vcassist-backend/lib/configutil"
-	configlibsql "vcassist-backend/lib/configutil/libsql"
 	"vcassist-backend/lib/serviceutil"
 	"vcassist-backend/lib/telemetry"
 	"vcassist-backend/proto/vcassist/services/keychain/v1/keychainv1connect"
 	"vcassist-backend/proto/vcassist/services/powerservice/v1/powerservicev1connect"
 	"vcassist-backend/services/powerservice"
-	powerservicedb "vcassist-backend/services/powerservice/db"
 
 	"connectrpc.com/connect"
 )
@@ -22,10 +20,9 @@ type OAuthConfig struct {
 }
 
 type Config struct {
-	BaseUrl         string              `json:"base_url"`
-	Database        configlibsql.Struct `json:"database"`
-	OAuth           OAuthConfig         `json:"oauth"`
-	KeychainBaseUrl string              `json:"keychain_service_baseurl"`
+	BaseUrl         string      `json:"base_url"`
+	OAuth           OAuthConfig `json:"oauth"`
+	KeychainBaseUrl string      `json:"keychain_service_baseurl"`
 }
 
 func main() {
@@ -41,10 +38,6 @@ func main() {
 	if err != nil {
 		serviceutil.Fatal("failed to read config", err)
 	}
-	db, err := config.Database.OpenDB(powerservicedb.Schema)
-	if err != nil {
-		serviceutil.Fatal("failed to open database", err)
-	}
 
 	otelIntercept := serviceutil.NewConnectOtelInterceptor()
 
@@ -52,7 +45,6 @@ func main() {
 	mux.Handle(powerservicev1connect.NewPowerschoolServiceHandler(
 		powerservicev1connect.NewInstrumentedPowerschoolServiceClient(
 			powerservice.NewService(
-				db,
 				keychainv1connect.NewKeychainServiceClient(
 					http.DefaultClient,
 					config.KeychainBaseUrl,
--- proto/vcassist/services/powerservice/v1/powerservicev1connect/api.telemetry.go
+++ proto/vcassist/services/powerservice/v1/powerservicev1connect/api.telemetry.go
@@ -159,37 +159,3 @@ func (c InstrumentedPowerschoolServiceClient) GetStudentData(ctx context.Context
 	return res, nil
 }
 
-func (c InstrumentedPowerschoolServiceClient) GetKnownCourses(ctx context.Context, req *connect.Request[v1.GetKnownCoursesRequest]) (*connect.Response[v1.GetKnownCoursesResponse], error) {
-	ctx, span := powerschoolServiceTracer.Start(ctx, "GetKnownCourses")
-	defer span.End()
-
-	if span.IsRecording() {
-		input, err := protojson.Marshal(req.Msg)
-		if err == nil {
-			span.SetAttributes(attribute.String("input", string(input)))
-		} else {
-			span.SetAttributes(attribute.String("input", "ERROR: FAILED TO SERIALIZE"))
-			span.RecordError(err)
-		}
-	}
-
-	res, err := c.inner.GetKnownCourses(ctx, req)
-	if err != nil {
-		span.RecordError(err)
-		span.SetStatus(codes.Error, err.Error())
-		return nil, err
-	}
-
-	if span.IsRecording() {
-		output, err := protojson.Marshal(res.Msg)
-		if err == nil {
-			span.SetAttributes(attribute.String("output", string(output)))
-		} else {
-			span.SetAttributes(attribute.String("output", "ERROR: FAILED TO SERIALIZE"))
-			span.RecordError(err)
-		}
-	}
-
-	return res, nil
-}
-
