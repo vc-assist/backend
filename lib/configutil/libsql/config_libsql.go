11affe1da6301b53fef4dbaf43e6d03279e962bc
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2024-09-02 17:14:09 -0700

    remove libsql from codebase

--- lib/configutil/libsql/config_libsql.go
+++ lib/configutil/libsql/config_libsql.go
@@ -4,18 +4,13 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
-	"time"
 	devenv "vcassist-backend/dev/env"
-
-	"github.com/tursodatabase/go-libsql"
 
 	_ "modernc.org/sqlite"
 )
 
 type Struct struct {
-	File      string `json:"file"`
-	Url       string `json:"url"`
-	AuthToken string `json:"auth_token"`
+	File string `json:"file"`
 }
 
 func (config Struct) OpenDB() (*sql.DB, error) {
@@ -27,45 +22,27 @@ func (config Struct) OpenDB() (*sql.DB, error) {
 		return nil, statErr
 	}
 
-	if config.Url == "" {
-		_, statErr = os.Stat(dbpath)
-		isNewDb := os.IsNotExist(statErr)
-		if isNewDb {
-			f, err := os.Create(dbpath)
-			if err != nil {
-				return nil, err
-			}
-			f.Close()
-		}
-
-		db, err := sql.Open("sqlite", dbpath)
+	_, statErr = os.Stat(dbpath)
+	isNewDb := os.IsNotExist(statErr)
+	if isNewDb {
+		f, err := os.Create(dbpath)
 		if err != nil {
 			return nil, err
 		}
-		// see this stackoverflow post for information on why the following
-		// lines exist: https://stackoverflow.com/questions/35804884/sqlite-concurrent-writing-performance
-		db.SetMaxOpenConns(1)
-		_, err = db.Exec("PRAGMA journal_mode=WAL")
-		if err != nil {
-			return nil, err
-		}
-
-		return db, nil
+		f.Close()
 	}
 
-	connector, err := libsql.NewEmbeddedReplicaConnector(
-		dbpath,
-		config.Url,
-		libsql.WithAuthToken(config.AuthToken),
-		libsql.WithReadYourWrites(true),
-		libsql.WithSyncInterval(time.Minute),
-	)
+	db, err := sql.Open("sqlite", dbpath)
 	if err != nil {
-		return nil, fmt.Errorf("create libsql connector: %w", err)
+		return nil, err
 	}
-
-	db := sql.OpenDB(connector)
+	// see this stackoverflow post for information on why the following
+	// lines exist: https://stackoverflow.com/questions/35804884/sqlite-concurrent-writing-performance
 	db.SetMaxOpenConns(1)
+	_, err = db.Exec("PRAGMA journal_mode=WAL")
+	if err != nil {
+		return nil, err
+	}
 
 	return db, nil
 }
